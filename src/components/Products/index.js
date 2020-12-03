8ac65f3c356b25d5777917955d91825f12d0457a
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-12-03 16:02:06 -0500

    fix header content

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
+
 import Product from './../Product'
+import useStyles from './styles';
 
 const Products = () => {
+  const classes = useStyles()
+  
   const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$150', image: 'https://assets.ajio.com/medias/sys_master/root/ajio/catalog/5f0f5ccaaeb26931758862c3/nike_grey_flex_experience_rn_9_lace-up_sports_shoes.jpg'},
     {id: 2, name: 'Macbook', description: 'Apple item.', price: '$2000', image: 'https://cdn.colombia.com/sdi/2016/10/19/apple-revelara-sus-nuevos-mac-el-27-de-octubre-522566.jpg'}
   ]
   return (
-    <main>
+    <main className={classes.content} >
+      <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {products.map(product => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
